28328a2a124bb0327fabff9e0d712eb39e9e345f
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2022-07-01 00:34:50 -0700

    Calendar scraper started

--- scrapers/meetings.py
+++ scrapers/meetings.py
@@ -1,14 +1,26 @@
 # %%
 import requests
 from bs4 import BeautifulSoup
-
+import urllib.request
+import ssl
+if hasattr(ssl, '_create_unverified_context'): # Should only be needed when developing locally
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 RSS_FEED_URL = "https://pattersonca.iqm2.com/Services/RSS.aspx?Feed=Calendar"
 PAGE_URL = "https://pattersonca.iqm2.com/Citizens/calendar.aspx"
 DIV = "ContentPlaceholder1_pnlMeetings"
 
-# %%
-r = requests.get(PAGE_URL)
-soup = BeautifulSoup(r.text, "html.parser")
-soup
+headers = {
+    'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+    'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml; q=0.9,*/*; q=0.8',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
+
+# %%
+r = urllib.request.urlopen(PAGE_URL) 
+soup = BeautifulSoup(r, "html.parser")
+calendar = soup.find("div", {"id":"ContentPlaceholder1_pnlMeetings"})
+
+
+# %%
