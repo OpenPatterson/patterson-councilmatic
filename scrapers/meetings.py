6dd2c813c29686b2655047bdae0b40af9021d28e
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2022-07-02 00:59:56 -0700

    iterating through calendar list

--- scrapers/meetings.py
+++ scrapers/meetings.py
@@ -1,4 +1,5 @@
 # %%
+from email import message_from_string
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
@@ -21,6 +22,21 @@ headers = {
 r = urllib.request.urlopen(PAGE_URL) 
 soup = BeautifulSoup(r, "html.parser")
 calendar = soup.find("div", {"id":"ContentPlaceholder1_pnlMeetings"})
+row_items = calendar.find_all("div",{"class":"Row MeetingRow"})
+row_items_alt = calendar.find_all("div",{"class":"Row MeetingRow Alt"})
 
+len(row_items), len(row_items_alt)
 
+# TODO:
+#     GET DATE OF MEETING 
+#     CANCELELED OR NOT? 
+#     TYPE OF MEETING. REGULAR OR SPECIAL MEETING 
+#     AGENDA LINK 
+#     AGENDA PACKET LINK 
+#     MINUTES LINK 
+#     SUMMARY LINK 
+# %%
+row_items[0]
+# %%
+row_items_alt[0]
 # %%
